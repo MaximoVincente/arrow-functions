eefb66a20493e1a7fef9195aea1af3256df70df2
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-09-17 15:11:36 -0400

    responded to question 1 of step 11

--- app.js
+++ app.js
@@ -184,17 +184,17 @@ Student.prototype.scope = function () {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scope();
+joe.scope();
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scopeArrow();
+joe.scopeArrow();
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// Student{name: 'Joe', age: 'Schmoe', hometown: 100} I'm assuming that the hometown is Schmoe and the age is 100, but because when the new instance joe was created, the argument 2 is Schmoe, and parameter 2 in the contstructor function Student is age.
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// 
 // 3. Explain why "this" is different when an arrow function is used.
 //
